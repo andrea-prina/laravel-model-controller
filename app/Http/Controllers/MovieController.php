9589babed133dd68f11d83be799e3e6f3046020e
andrea-prina <and.prina@gmail.com>
2022-09-08 16:46:21 +0200

    Create movies db model and controller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index(){
-        return view('movies.index');
+        $movies = Movie::first();
+
+        return view('movies.index', compact('movies'));
     }
 }
