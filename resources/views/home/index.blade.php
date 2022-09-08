8319d785d15052813fb377f1ab8a14ffbeb36fdd
andrea-prina <and.prina@gmail.com>
2022-09-08 17:01:21 +0200

    Create basic homepage

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('main-content')
+
+    <div class="container d-flex justify-content-center align-items-center p-5">
+        <h1>MOVIES HOMEPAGE</h1>
+    </div>
+
+@endsection
